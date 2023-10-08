e5c30b0221abe695c1db6a8097f1705c960d1e7c
vinagunawan <vinaaantaristagunawan@gmail.com>
2023-10-08 11:30:49 +0800

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT||3000;
 const bodyParser = require('body-parser');
-const {dataTracking} = require("./models/dataTracking");
+const {dataTracking} = require("./models");
 const cors = require("cors");
 
 
